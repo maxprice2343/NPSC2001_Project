a4e4e08cad2765be15f78237bfeb79b48b190afd
maxprice2343 <20916080@student.curtin.edu.au>
2023-06-28 22:52:28 +0800

    Added reset method and helper methods

--- environment/proj_env.py
+++ environment/proj_env.py
@@ -3,13 +3,13 @@ from gymnasium import spaces
 import numpy as np
 
 WINDOW_SIZE = 512
-NUM_NODES = 4
 
 class ProjEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": 4}
 
-    def __init__(self, render_mode=None, size=10):
+    def __init__(self, render_mode=None, size=10, num_nodes=4):
         self.size = size
+        self.num_nodes = num_nodes
         self.window_size = WINDOW_SIZE
 
         # Observations consist of agent location, node locations, and currently
@@ -18,7 +18,7 @@ class ProjEnv(gym.Env):
             {
                 "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
                 "nodes": spaces.Sequence(spaces.Box(0, size - 1, shape=(2,), dtype=int)),
-                "active": spaces.MultiBinary(NUM_NODES)
+                "active": spaces.MultiBinary(num_nodes)
             }
         )
         # Agent can move up, down, left, or right
@@ -32,6 +32,36 @@ class ProjEnv(gym.Env):
         self.window = None
         self.clock = None
     
+    # Initiates a new episode.
+    def reset(self, seed=None):
+        # Seeds the random number generator
+        super().reset(seed=seed)
+
+        # Randomly generates the agent location in the form [x,y]
+        self._agent_location = self._generate_random_position()
+
+        # Randomly generates node locations until they are not equal to the agent location
+        # or other node locations
+        for i in range(self.num_nodes):
+            self._node_locations[i] = self._generate_random_position(
+                excludes=(self._agent_location + self._node_locations[:i])
+            )
+        
+        observation = self._get_obs()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation
+
+    def _generate_random_position(self, excludes=[]):
+        location = self.np_random.integers(0, self.size, size=2, dtype=int)
+
+        while location in excludes:
+            location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        
+        return location
+
     def _get_obs(self):
         return {
             "agent": self._agent_location,
@@ -40,8 +70,7 @@ class ProjEnv(gym.Env):
         }
 
     def _get_distance(self, node_number):
-        return np.linalg.norm(self._agent_location - self._node_locations)
-        return
+        return np.linalg.norm(self._agent_location - self._node_locations[node_number])
 
     def _action_to_direction(self, action):
         """
