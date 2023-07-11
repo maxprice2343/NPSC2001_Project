5e7a653a8c51d6839b7f13b9e58db1081560041e
maxprice2343 <20916080@student.curtin.edu.au>
2023-07-11 16:10:29 +0800

    Created driver code in train.py

--- network_agent/train.py
+++ network_agent/train.py
@@ -1,49 +1,26 @@
 import gymnasium as gym
-from dqn_agent import DqnAgent
-from replay_buffer import ReplayBuffer
+from dqn_agent import dqn_agent
+from replay_buffer import replay_buffer
 import gym_examples
 
-def collect_gameplay_experience(env, agent, buffer):
-    """
-    Initiates an episode of the environment. Actions are generated based on the
-    agent's policy. The gameplay experience is stored in the buffer to be used
-    for training.
-    """
-    # Initializes the environment and gets the initial environment
-    state, _ = env.reset()
-    done = False
+GAMMA = 1
+EPSILON = 0.1
+NUM_EPISODES = 1000
+REPLAY_BUFFER_SIZE = 300
+BATCH_SIZE = 100
 
-    # Iterates until the environment is complete
-    while not done:
-        # Uses the agent's policy to produce an action given the current state
-        action = agent.policy(state)
-        # Applies the generated action to the environment
-        next_state, reward, done, _, _= env.step(action)
-        # Stores the gameplay experience (original state, resultant state,
-        # reward, action taken, and done) in the buffer
-        buffer.store_gameplay_experience(state, next_state, reward, action, done)
-        state = next_state
+agent = dqn_agent(
+    "Network-v0",
+    4,
+    4,
+    GAMMA,
+    EPSILON,
+    NUM_EPISODES,
+    REPLAY_BUFFER_SIZE,
+    BATCH_SIZE
+)
+agent.training_episodes()
+print(agent.sum_rewards_episode)
 
-def train_model(max_episodes=50000):
-    """
-    Trains a DQN agent to optimize the network time efficiency 
-    """
-    env = gym.make("Network-v0")
-    agent = DqnAgent((4,), (4,))
-    buffer = ReplayBuffer()
-
-    for episode_count in range(max_episodes):
-        # Starts 1 environment episode
-        collect_gameplay_experience(env, agent, buffer)
-        # Collects a random sample of gameplay experiences
-        gameplay_experience_batch = buffer.sample_gameplay_batch()
-        loss = agent.train(gameplay_experience_batch)
-        # Updates the target network every 20 episodes
-        if episode_count % 20 == 0:
-            agent.update_target_network()
-    
-    env.close()
-
-if __name__ == '__main__':
-    train_model()
-    
+agent.main_network.summary()
+agent.main_network.save("trained_model_temp.h5")
