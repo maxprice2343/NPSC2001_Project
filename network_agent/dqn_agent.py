6049cb24e6609f6fe87a4eefd7c222c32bc590a7
maxprice2343 <20916080@student.curtin.edu.au>
2023-07-11 11:19:12 +0800

    Started new dqn_agent class

--- network_agent/dqn_agent.py
+++ network_agent/dqn_agent.py
@@ -1,60 +1,38 @@
-import tensorflow as tf
+"""
+Defines the dqn_agent class that creates the neural network.
+Adapted from https://aleksandarhaber.com/deep-q-networks-dqn-in-python-from-scratch-by-using-openai-gym-and-tensorflow-reinforcement-learning-tutorial/
+"""
+
 import numpy as np
+import tensorflow as tf
+import gymnasium as gym
+import gym_examples
+from replay_buffer import transition, replay_buffer
 
-class DqnAgent:
-    def __init__(self, state_shape, action_shape):
-        self.state_shape = state_shape
-        self.action_shape = action_shape
-        self.q_net = self._build_dqn_model()
-        self.target_q_net = self._build_dqn_model()
+UPDATE_TARGET_NETWORK_PERIOD = 100
 
-    def policy(self, state: dict):
-        """
-        Takes an environment state and returns an action that the agent should
-        take
-        """
-        # Concatenates the agent and active arrays into a 1 dimensional numpy
-        # array
-        con = np.concatenate((state['agent'], state['active']))
-        # Converts the numpy array representing the environment state to a
-        # tensor
-        con = np.reshape(con, (1, 4))
-        state_input = tf.convert_to_tensor(con, dtype=tf.float32)
-        action_q = self.q_net(state_input)
-        action = np.argmax(action_q.numpy()[0], axis=0)
-        return action
+class dqn_agent:
+    """
+    
+    """
+    def __init__(self, env_name, state_dimension, action_dimension, gamma,
+                 epsilon, num_episodes, replay_buffer_size, batch_size):
+        self.env = gym.make(env_name)
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.num_episodes = num_episodes
 
-    def train(self, batch):
-        """
-        Trains the DQN agent based on a batch of gameplay experiences
-        """
-        state_batch, next_state_batch, action_batch, reward_batch, done_batch = batch
-        print(f"state_batch:{state_batch}\n")
-        current_q = self.q_net(state_batch)
-        target_q = np.copy(current_q)
-        next_q = self.target_q_net(next_state_batch)
-        max_next_q = np.amax(next_q, axis=1)
-        for i in range(state_batch.shape[0]):
-            target_q[i][action_batch[i]] = reward_batch[i] if done_batch[i] else reward_batch[i] + 0.95 * max_next_q[i]
-        result = self.q_net.fit(x=state_batch, y=target_q)
+        self.state_dimension = state_dimension
+        self.action_dimension = action_dimension
 
-        return result.history['loss']
+        self.replay_buffer = replay_buffer(replay_buffer_size, batch_size)
 
-    def _build_dqn_model(self):
-        q_net = tf.keras.Sequential()
-        q_net.add(
-            tf.keras.Input(shape=self.state_shape)
-        )
-        q_net.add(
-            tf.keras.layers.Dense(64, activation='relu', kernel_initializer='he_uniform')
-        )
-        q_net.add(
-            tf.keras.layers.Dense(32, activation='relu', kernel_initializer='he_uniform')
-        )
-        q_net.add(
-            tf.keras.layers.Dense(len(self.action_shape), activation='linear', kernel_initializer='he_uniform')
-        )
+        self.update_target_network_period = UPDATE_TARGET_NETWORK_PERIOD
+        self.update_target_network_count = 0
 
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-        q_net.compile(optimizer=optimizer, loss='mse')
-        return q_net
+        self.main_network = self.create_network()
+        self.target_network = self.create_network()
+        self.target_network.set_weights(self.main_network.get_weights())
+
+        self.sum_rewards_episode = []
+        self.actions_append = []
