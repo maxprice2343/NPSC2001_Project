e0f9bd3eb39a924da560ac175802b302f6efe201
maxprice2343 <20916080@student.curtin.edu.au>
2023-07-30 18:56:05 +0800

    Updated training parameters and retrained model

--- network_agent/train.py
+++ network_agent/train.py
@@ -4,7 +4,7 @@ from replay_buffer import replay_buffer
 import gym_examples
 
 GAMMA = 1
-EPSILON = 0.2
+EPSILON = 0.1
 NUM_EPISODES = 100
 REPLAY_BUFFER_SIZE = 300
 BATCH_SIZE = 100
@@ -24,5 +24,5 @@ agent = dqn_agent(
 agent.training_episodes()
 print(agent.sum_rewards_episode)
 
-agent.main_network.summary()
-agent.main_network.save("trained_model_temp.h5")
+agent.target_network.summary()
+agent.target_network.save("trained_model_temp.h5")
