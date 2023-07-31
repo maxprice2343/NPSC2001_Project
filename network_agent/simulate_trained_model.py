825393c8056dea68183bc8f81e5a0235ae25c1ce
maxprice2343 <20916080@student.curtin.edu.au>
2023-07-31 15:55:02 +0800

    Added functionality to receive model path from command line. Removed video functionality, environment can be rendered in real time in pygame.

--- network_agent/simulate_trained_model.py
+++ network_agent/simulate_trained_model.py
@@ -3,30 +3,27 @@ import tensorflow as tf
 import gymnasium as gym
 from dqn_agent import dqn_agent
 import pathlib
+import sys
 
-# Path to the trained model
-model_path = pathlib.Path("trained_model_temp.h5")
+try:
+    path = sys.argv[1]
+    model_path = pathlib.Path(path)
+    model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={'loss':dqn_agent.loss}
+    )
+except IndexError:
+    print("Model path not provided")
+except OSError:
+    print("Invalid path provided")
+else:
+    # Creates the environment
+    env = gym.make("Network-v0", size=10, render_mode="human")
 
-# Loads the model from the file
-model = tf.keras.models.load_model(
-    model_path,
-    custom_objects={'loss':dqn_agent.loss}
-)
+    # Initializes the environment
+    current_state, _ = env.reset()
 
-# Creates the environment
-env = gym.make("Network-v0", size=5, render_mode="rgb_array")
-
-# Initializes the environment
-current_state, _ = env.reset()
-
-# Sets up the environment to record
-video_length = 400
-env = gym.wrappers.RecordVideo(env, 'stored_video', video_length=video_length)
-
-# Steps until the environment is in a terminal state
-for _ in range(10):
     terminal_state = False
-    sum_obtained_rewards = 0
     while not terminal_state:
         # Uses the model to generate q values based on the current state
         q_values = model.predict(current_state[None, :])
@@ -36,7 +33,4 @@ for _ in range(10):
             np.where(q_values[0,:]==np.max(q_values[0,:]))[0]
         )
         current_state, reward, terminal_state, _, _ = env.step(action)
-        sum_obtained_rewards += reward
-    current_state, _ = env.reset()
-
-env.close()
+    env.close()
