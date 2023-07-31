7744bfd18e5151d4aa963352137d2fa2f0772b66
maxprice2343 <20916080@student.curtin.edu.au>
2023-07-31 13:04:19 +0800

    Increased number of episodes in training and retrained model.

--- network_agent/train.py
+++ network_agent/train.py
@@ -5,7 +5,7 @@ import gym_examples
 
 GAMMA = 1
 EPSILON = 0.1
-NUM_EPISODES = 100
+NUM_EPISODES = 500
 REPLAY_BUFFER_SIZE = 300
 BATCH_SIZE = 100
 STATE_DIMENSION = 4
@@ -26,5 +26,5 @@ agent = dqn_agent(
 agent.training_episodes()
 print(agent.sum_rewards_episode)
 
-agent.target_network.summary()
-agent.target_network.save("trained_model_temp.h5")
+agent.main_network.summary()
+agent.main_network.save("trained_model_temp.h5")
