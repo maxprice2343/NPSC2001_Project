fb30be7b793dc5b41296187e16848b861d3cec2c
maxprice2343 <20916080@student.curtin.edu.au>
2023-08-11 11:06:16 +0800

    Added node class allowing for customizing nodes

---
+++ gym_examples/node.py
@@ -0,0 +1,22 @@
+"""
+Defines a node class that can store information about a node.
+This allows an environment to define multiple nodes of different types.
+"""
+
+import numpy as np
+
+class node:
+    def __init__(self, location, range, time):
+        self.range = range
+        self.location = location
+        self.time = time
+        self.active = False
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def within_range(self, location):
+        return np.linalg.norm(self.location, location) <= range
